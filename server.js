337265b4f9bd12bee33282830d24e30bbfb4c6d7
i1rakesh <rk7290948939@gmail.com>
2025-12-07 19:30:15 +0530

    added the new vercel web hosted link in allowed origins

--- server.js
+++ server.js
@@ -13,11 +13,21 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://file-sharing-frontend-delta.vercel.app",
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 
 app.use(express.json());
 app.use(morgan('dev'));
